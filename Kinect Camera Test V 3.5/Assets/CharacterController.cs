17f0f7d63f060440714ce117f1bdd2033312f3a6
Herb Guzman <55105714+GMZRD71@users.noreply.github.com>
2019-11-21 18:36:39 -0600

    KinectDK integration with virtual AI Lab Environment in Unity
    This is shows the capability of using one body tracker based on the Azure Kinect DK depth sensor and Unity Game Engine in a virtual room

---
+++ Kinect Camera Test V 3.5/Assets/CharacterController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// Main Class = Same as File Name: CharacterController
+public class CharacterController : MonoBehaviour
+{
+    // Public exposed variable - speed
+    // How fast the charater responds to the keyboard movements
+    // or the controller movements
+    public float speed = 10.0F;
+
+    // Ise this for initialization
+    // FUNCTION - Start
+    void Start()
+    {
+        // This turns off the cursor and keeps it inside the 
+        // game window
+        // Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // FUNTION - Update is called once per frame
+    // An update loop does not run at exactly the same
+    // time during each iteration of the loop, depending
+    // on what's happening in the game
+    void Update()
+    {
+        // "Vertical" and "Horizontal" are global settings from UNITY to define what
+        // the keyboard, joystick, etc. will do...
+        float translation = Input.GetAxis("Vertical") * speed;  // Forwards and backwards movement
+        float straffe = Input.GetAxis("Horizontal") * speed;    // Side-side movements
+
+        // Movement of the character: translate: multipiply by deltaTime to keep the movements
+        // smooth and keep up with the Update FUNCTION
+        translation *= Time.deltaTime;  // The delta-time is the time interval between updates
+        straffe *= Time.deltaTime;      // Using this delta-time to be in sync with the updates
+
+        // Straffe (side-side) along the x-axis and translate along the z axis
+        transform.Translate(straffe, 0, translation);
+
+        // If we hit the escape key, the cursor will turn back on
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+}
---
+++ Kinect Camera Test V 3.5/Assets/camMouseLook.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// CLASS - camMouseLook
+
+public class camMouseLook : MonoBehaviour
+{
+    // This keeps track of how movement the camera has made
+    // since the last update; we need to keep track of the
+    // total movement so we can add it to the controller
+    Vector2 mouseLook;
+    Vector2 smoothV;   // This helps smooth out the mouse movement
+    public float sensitivity = 5.0f;  // Mouse sensitivity
+    public float smoothing = 2.0f;    // How much movement we want
+
+    GameObject character;   // This points back to the camera
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = this.transform.parent.gameObject;  // The parent in this case is the camera
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Allows control by getting the movement of the mouse
+        // md = "mouse delta"
+        // Then multiply the change in mouse movement (delta) by the smoothing and 
+        // sensitivity values
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing) ;   // Slows it down and Lerps = Linear Interpolation of Movement 
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing) ;   // Lerp is very useful when moving between one point and another smoothly
+        mouseLook += smoothV;                                       // rather than "snapping" from point to point
+        // Adding operator of all the operations
+        
+        // Add the mouse look y as a local rotation to the camera along its right axis
+        // the "-" is an inverted system
+        // rotation about the y-axis with mouse UP-DOWN
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        // Rotate about the character's upward movement, not the camera, because we want the whole
+        // character to move - MOUSE SIDE-SIDE
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
